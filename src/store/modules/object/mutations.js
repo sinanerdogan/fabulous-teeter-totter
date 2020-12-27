d5fa2de6ee365adbb4a3b010961fe34176223ef5
Sinan Erdoğan <sinaner92@gmail.com>
2020-12-27 17:48:57 +0300

    Typeadded

--- src/store/modules/object/mutations.js
+++ src/store/modules/object/mutations.js
@@ -4,13 +4,16 @@ import { helpers } from "../../../utils";
 const mutations = {
 	generateObject({ elements }, isRandom = false) {
 		console.log(elements, isRandom);
+		const typeNumber = helpers.randomNumberGenerator(1, 3);
 		const id = helpers.randomIdGenerator();
 		const weight = helpers.randomNumberGenerator(objects.MIN_WEIGHT, objects.MAX_WEIGHT);
+		const type = objects.TYPES[typeNumber];
 		const color = helpers.randomColorGenerator();
 		const size = 1 + objects.SIZE_SCALE_RATIO * (weight - 1);
 		const position = 0;
 		const isDropped = false;
-		elements.push({ id, weight, color, size, position, isRandom, isDropped });
+		elements.push({ id, type, weight, color, size, position, isRandom, isDropped });
+		console.log({ id, type, weight, color, size, position, isRandom, isDropped });
 	}
 };
 
